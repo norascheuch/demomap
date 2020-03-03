c7d40e40ce5fd00298c11977003afb022066c377
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-03-03 16:34:31 +0100

    Pundit set up for demos. for users that are defined as organifer of a specific demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:splash, :index, :show]
+
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
---
+++ db/migrate/20200303133731_add_organizer_to_users.rb
@@ -0,0 +1,6 @@
+tails db:migrate
+class AddOrganizerToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :organizer, :boolean
+  end
+end
