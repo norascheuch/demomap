e9bc4914c8134d3a62254ae802ad691edc5c6751
xenos <xenos@reinstraum.net>
2020-03-03 17:27:17 +0100

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,15 +18,4 @@ class EventsController < ApplicationController
   def show
 
   end
-
-  private
-
-  def set_marker_img(event,event_img)
-     {
-     lat: event.latitude,
-     lng: event.longitude,
-     infoWindow: render_to_string(partial: "info_window", locals: { event: event }),
-     image_url: helpers.asset_url(event_img)
-     }
-  end
 end
