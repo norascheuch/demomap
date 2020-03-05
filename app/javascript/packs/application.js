fc98ce9ce91d498b2a9a18429c7ca584d17e44a5
Marie Hess <hess.marietheres@gmail.com>
2020-03-05 14:29:08 +0100

    route

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,4 +4,4 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 
-initMapbox();
+const map = initMapbox();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,17 +17,19 @@ const fitMapToMarkers = (map, markers) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/light-v10'
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-122.662323, 45.523751],
+      zoom: 12,
     });
 
-    map.addControl(
-    new mapboxgl.GeolocateControl({
-    positionOptions: {
-    enableHighAccuracy: true
-    },
-    trackUserLocation: true
-    })
-    );
+    // map.addControl(
+    // new mapboxgl.GeolocateControl({
+    // positionOptions: {
+    // enableHighAccuracy: true
+    // },
+    // trackUserLocation: true
+    // })
+    // );
 
   const markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach((marker) => {
@@ -47,9 +49,104 @@ const fitMapToMarkers = (map, markers) => {
       .addTo(map);
   });
   fitMapToMarkers(map, markers);
+
+  var start = [-122.662323, 45.523751];
+  var end = [-122.677738,45.522458];
+
+  const getRoute = (start, end) => {
+    // make a directions request using cycling profile
+    // an arbitrary start will always be the same
+    // only the end or destination will change
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+    // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onload = function() {
+      var json = JSON.parse(req.response);
+      var data = json.routes[0];
+      var route = data.geometry.coordinates;
+      var geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: route
+        }
+      };
+      // if the route already exists on the map, reset it using setData
+      if (map.getSource('route')) {
+        map.getSource('route').setData(geojson);
+      } else { // otherwise, make a new request
+        map.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: geojson
+              }
+            }
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#3887be',
+            'line-width': 5,
+            'line-opacity': 0.75
+          }
+        });
+      }
+      // add turn instructions here at the end
+    };
+    req.send();
+
+    var bounds = [[-123.069003, 45.395273], [-122.303707, 45.612333]];
+    map.setMaxBounds(bounds);
+
+    // initialize the map canvas to interact with later
+    var canvas = map.getCanvasContainer();
   }
+
+  map.on('load', function() {
+    // make an initial directions request that
+    // starts and ends at the same location
+    getRoute(start, end);
+
+    // Add starting point to the map
+    map.addLayer({
+      id: 'point',
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [{
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: start
+            }
+          }
+          ]
+        }
+      },
+      paint: {
+        'circle-radius': 10,
+        'circle-color': '#3887be'
+      }
+    });
+    // this is where the code from the next step will go
+  });
+
+  }
+
 };
-
-
 
 export { initMapbox };
