59f1fc49f53d7246287fd59616f22d6f1e310eba
Marie Hess <hess.marietheres@gmail.com>
2020-03-05 16:33:44 +0100

    Map-route included

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @events = policy_scope(Event.geocoded)
+    @mappoints = '13.43,52.51;13.42,52.5;13.41,52.5'
     @markers = @events.map do |event|
       @icon = event.event_type.icon
       @demo = event.demo
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,63 +1,25 @@
 import mapboxgl from 'mapbox-gl';
 
-
-
-
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
-};
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: [-122.662323, 45.523751],
-      zoom: 12,
-    });
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    console.log(bounds)
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
+  };
 
-    // map.addControl(
-    // new mapboxgl.GeolocateControl({
-    // positionOptions: {
-    // enableHighAccuracy: true
-    // },
-    // trackUserLocation: true
-    // })
-    // );
-
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
-  const element = document.createElement('div');
-  element.className = 'marker';
-  element.style.backgroundImage = `url('${marker.image_url}')`;
-  element.style.backgroundSize = 'contain';
-  element.style.width = '25px';
-  element.style.height = '25px';
-
-
-    new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
-  });
-  fitMapToMarkers(map, markers);
-
-  var start = [-122.662323, 45.523751];
-  var end = [-122.677738,45.522458];
-
-  const getRoute = (start, end) => {
+  const getRoute = (map, mappoints) => {
     // make a directions request using cycling profile
     // an arbitrary start will always be the same
     // only the end or destination will change
-    var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/walking/' + mappoints + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
     // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
     var req = new XMLHttpRequest();
     req.open('GET', url, true);
@@ -77,7 +39,7 @@ const fitMapToMarkers = (map, markers) => {
       if (map.getSource('route')) {
         map.getSource('route').setData(geojson);
       } else { // otherwise, make a new request
-        map.addLayer({
+        const routeLayer = map.addLayer({
           id: 'route',
           type: 'line',
           source: {
@@ -87,7 +49,7 @@ const fitMapToMarkers = (map, markers) => {
               properties: {},
               geometry: {
                 type: 'LineString',
-                coordinates: geojson
+                coordinates: route
               }
             }
           },
@@ -105,48 +67,79 @@ const fitMapToMarkers = (map, markers) => {
       // add turn instructions here at the end
     };
     req.send();
-
-    var bounds = [[-123.069003, 45.395273], [-122.303707, 45.612333]];
-    map.setMaxBounds(bounds);
-
-    // initialize the map canvas to interact with later
-    var canvas = map.getCanvasContainer();
   }
 
-  map.on('load', function() {
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-122.662323, 45.523751],
+      zoom: 12,
+    });
+
+    map.on('load', function() {
     // make an initial directions request that
     // starts and ends at the same location
-    getRoute(start, end);
 
-    // Add starting point to the map
-    map.addLayer({
-      id: 'point',
-      type: 'circle',
-      source: {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: [{
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'Point',
-              coordinates: start
-            }
-          }
-          ]
-        }
+
+      map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
       },
-      paint: {
-        'circle-radius': 10,
-        'circle-color': '#3887be'
-      }
+      trackUserLocation: true
+      })
+      );
+
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+
+        new mapboxgl.Marker(element)
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup)
+          .addTo(map);
+      });
+      fitMapToMarkers(map, markers);
+
+      const mappoints = JSON.parse(mapElement.dataset.mappoints);
+      getRoute(map, mappoints);
+
+      // Add starting point to the map
+  //     map.addLayer({
+  //       id: 'point',
+  //       type: 'circle',
+  //       source: {
+  //         type: 'geojson',
+  //         data: {
+  //           type: 'FeatureCollection',
+  //           features: [{
+  //             type: 'Feature',
+  //             properties: {},
+  //             geometry: {
+  //               type: 'Point',
+  //               coordinates: start
+  //             }
+  //           }
+  //           ]
+  //         }
+  //       },
+  //       paint: {
+  //         'circle-radius': 10,
+  //         'circle-color': '#3887be'
+  //       }
+  //     });
     });
-    // this is where the code from the next step will go
-  });
-
   }
-
 };
 
 export { initMapbox };
