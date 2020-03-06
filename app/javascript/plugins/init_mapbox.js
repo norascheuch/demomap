11f9dfb94c11a5b969d70ad1b9513010e57830d8
Marie Hess <hess.marietheres@gmail.com>
2020-03-06 12:07:23 +0100

    style route

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -58,8 +58,8 @@ const initMapbox = () => {
             'line-cap': 'round'
           },
           paint: {
-            'line-color': '#3887be',
-            'line-width': 5,
+            'line-color': '#326ecd',
+            'line-width': 8,
             'line-opacity': 0.75
           }
         });
