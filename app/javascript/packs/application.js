02b22ed696134dc605a6a5d323144a529e8ed9a6
Marie Hess <hess.marietheres@gmail.com>
2020-03-09 19:48:39 +0100

    changes to pop up window and list view including not finished and out commented JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { toggleShadow } from '../plugins/footer_menu';
 import { commentUpdate } from '../plugins/comment_update';
+import { hiddenHeader } from '../plugins/event_header';
 
 
 initMapbox();
 toggleShadow();
+hiddenHeader();
 commentUpdate();  // this is left here to discuss with santi, it can be deleted (inclusive the corresponding file) at the end of march latest
 
---
+++ app/javascript/plugins/event_header.js
@@ -0,0 +1,16 @@
+// const hiddenHeader = () => {
+//   const pillEvent = document.getElementById("pills-event-tab");
+//   const mapEvent = document.getElementById("pills-map-tab");
+//   const header = document.getElementById("header");
+//   pillEvent.addEventListener('focus', () => {
+//     header.style.display = "block";
+//   });
+//   mapEvent.addEventListener('focus', () => {
+//     header.style.display = "none";
+//   });
+// }
+// export { hiddenHeader };
+
+
+
+//////do not delete!!!
