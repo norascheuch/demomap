0ac1e4c80aeb69b0a732f97971e28c30a8e6c911
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-03-10 16:39:07 +0100

    Twitter feed with hardcooded username

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -6,6 +6,15 @@ class TwitterApi
     client.user_timeline("BBCNews", count: 10, exclude_replies: true, include_rts: false)
   end
 
+### It'll be complicated to monitor.... ###
+  # def self.demo_hashtags
+  #   client.search("to: <% @demos.name %>", result_type: "recent").take(5).collect do |tweet|
+  #   "#{tweet.user.screen_name}: #{tweet.text}"
+  #   end
+  # end
+
+### END ###
+
   def self.client
     Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['YOUR_CONSUMER_KEY']
