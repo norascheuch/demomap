2db24a2906df31f93a4007cc4f0dd951ffefd545
Marie Hess <hess.marietheres@gmail.com>
2020-03-10 10:22:27 +0100

    changes Javascript

--- app/javascript/plugins/event_header.js
+++ app/javascript/plugins/event_header.js
@@ -1,15 +1,15 @@
-// const hiddenHeader = () => {
-//   const pillEvent = document.getElementById("pills-event-tab");
-//   const mapEvent = document.getElementById("pills-map-tab");
-//   const header = document.getElementById("header");
-//   pillEvent.addEventListener('focus', () => {
-//     header.style.display = "block";
-//   });
-//   mapEvent.addEventListener('focus', () => {
-//     header.style.display = "none";
-//   });
-// }
-// export { hiddenHeader };
+const hiddenHeader = () => {
+  const pillEvent = document.getElementById("pills-event-tab");
+  const mapEvent = document.getElementById("pills-map-tab");
+  const header_event = document.getElementById("header_event");
+  pillEvent.addEventListener('focus', () => {
+    header.style.display = "block";
+  });
+  mapEvent.addEventListener('focus', () => {
+    header.style.display = "none";
+  });
+}
+export { hiddenHeader };
 
 
 
