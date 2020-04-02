7572c371be75a32ae76e9cb5c2595d2b01cde222
Nora Scheuch <xenos@reinstraum.net>
2020-04-02 11:34:22 +0200

    increased selenium timeout and travis config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ Capybara.save_path = Rails.root.join('tmp/capybara')
 Capybara.current_driver = :headless_chrome
 Capybara.javascript_driver = :headless_chrome
 Capybara::Screenshot.autosave_on_failure = true
-Capybara.default_max_wait_time = 150
+Capybara.default_max_wait_time = 15
 
 # to manually save screenshots: screenshot_and_save_page
 
