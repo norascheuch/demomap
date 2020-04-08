e61de19e6c6a5f1a9e2f1791533b3acb9e74861f
Nora Scheuch <xenos@reinstraum.net>
2020-04-08 10:05:40 +0200

    clean up leaflet main map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,18 +6,19 @@ import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import "../plugins/flatpickr"
 
 
-// import { initMapbox } from '../plugins/init_mapbox';
+import { initMapbox } from '../plugins/init_mapbox';
 import { toggleShadow } from '../plugins/footer_menu';
 import { hiddenHeader } from '../plugins/event_header';
 import { readMore } from '../plugins/read_more';
-import { leafletMap } from '../plugins/leaflet_map';
+import { leafletMainMap } from '../plugins/leaflet_main_map';
 // import { readMore } from '../plugins/add_to_home_screen.js';
 
 
-// initMapbox();
+initMapbox();
 toggleShadow();
 hiddenHeader();
 readMore();
-leafletMap();
+leafletMainMap();
 // add_to_home_screen();
+
 
