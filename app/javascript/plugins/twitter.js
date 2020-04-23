a8e8e2a0c18abdce47df9c70b1931f122b4e5836
Nora Scheuch <xenos@reinstraum.net>
2020-04-23 18:06:13 +0200

    testing env

--- app/javascript/plugins/twitter.js
+++ app/javascript/plugins/twitter.js
@@ -1,22 +1,23 @@
-// var Twitter = require('twitter');
+var Twitter = require('twitter');
 
-// const twitterFeed = () => {
-//   const twitterButton = document.getElementById('pills-twitter')
-//   console.log(process.env.YOUR_CONSUMER_KEY)
-//   if (twitterButton) {
-//     var twitterClient = new Twitter({
-//       consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//       consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//       access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//       access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-//     });
+const twitterFeed = () => {
+  const twitterButton = document.getElementById('pills-twitter')
+  if (twitterButton) {
+  console.log(process.env.CLOUDINARY_URL)
+  }
+  // if (twitterButton) {
+  //   var twitterClient = new Twitter({
+  //     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  //     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  //     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  //     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  //   });
 
 
 
-//     twitterButton.addEventListener('click', () => {
+  //   twitterButton.addEventListener('click', () => {
 
-//     } )
-//   }
-// }
+  //   } )
+}
 
-// export { twitterFeed };
+export { twitterFeed };
